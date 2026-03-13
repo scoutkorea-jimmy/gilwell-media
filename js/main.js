f56db0c2af8fcae7813507a86238c24a852d02d3
Jimmy <scoutkorea@kakao.com>
2026-03-14 07:50:06 +0900

    chore: align version labels to v0.013.01

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.012.00';
+  GW.APP_VERSION = '0.013.01';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'worm', 'people'];
 
