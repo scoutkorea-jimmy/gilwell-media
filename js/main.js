f046216748ad6b17975ac9a58cf2ce7172692ec8
Jimmy <scoutkorea@kakao.com>
2026-03-14 17:36:45 +0900

    fix: move glossary add flow into modal

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.048.09';
+  GW.APP_VERSION = '0.048.10';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'wosm', 'people'];
 
