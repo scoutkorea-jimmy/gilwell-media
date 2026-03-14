b10710370026b4b69f4695a0c9c8a1a28b47c519
Jimmy <scoutkorea@kakao.com>
2026-03-14 17:27:37 +0900

    fix: polish glossary description panel

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.048.06';
+  GW.APP_VERSION = '0.048.07';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'wosm', 'people'];
 
