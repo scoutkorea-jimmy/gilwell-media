cc1fda4a1af9a9d8d9f948d79aa980a1f2f286b1
Jimmy <scoutkorea@kakao.com>
2026-03-14 10:24:15 +0900

    feat: group board titles with post counts

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.024.00';
+  GW.APP_VERSION = '0.025.00';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'worm', 'people'];
 
