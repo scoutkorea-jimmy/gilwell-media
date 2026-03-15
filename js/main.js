02244980324706feba8808083ea0ffb04a214c33
Jimmy <scoutkorea@kakao.com>
2026-03-15 10:26:28 +0900

    Simplify admin top navigation

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.054.02';
+  GW.APP_VERSION = '0.054.03';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'wosm', 'people'];
 
