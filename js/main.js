f3dc0ddfeeb48804da6ecaa9e91e0cbc98a79cc7
Jimmy <scoutkorea@kakao.com>
2026-03-15 15:12:29 +0900

    Add gradient footer styling

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
   'use strict';
 
   const GW = window.GW = {};
-  GW.APP_VERSION = '0.055.03';
+  GW.APP_VERSION = '0.055.04';
   GW.EDITOR_LETTERS = ['A', 'B', 'C'];
   GW.TAG_CATEGORIES = ['korea', 'apr', 'wosm', 'people'];
 
